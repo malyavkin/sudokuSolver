c8380ae8233012a21346966fd7974eee9237b375
malyavkin <a@malyavk.in>
2016-10-28 02:59:37 +0300

    1

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,20 @@
+import copy
+import sample
+
+
+def print_puzzle(puzzle):
+    delim = "|"
+    for i in range(len(puzzle)):
+        row = puzzle[i]
+        print("{}   {}   {}".format(delim.join(row[0:3]), delim.join(row[3:6]), delim.join(row[6:9])))
+        if i % 3 == 2:
+            print()
+
+
+def get_region_indexes(region_number, region_size):
+    return region_number*region_size, (region_number+1)*region_size
+
+
 def get_section(i):
     """
     get section indexes for current index
@@ -11,7 +28,8 @@ def get_section(i):
     :return:
     """
     section = i // 3
-    return section * 3, section * 3 + 3
+    return get_region_indexes(section, 3)
+
 
 
 def get_row(puzzle, row):
@@ -34,8 +52,122 @@ def get_column(puzzle, column):
     return [row[column] for row in puzzle]
 
 
-def get_region(puzzle, row, column):
+def get_region(puzzle, reg_row, reg_column):
+    row_section = get_region_indexes(reg_row, 3)
+    col_section = get_region_indexes(reg_column, 3)
+    region = [item for row in puzzle[slice(*row_section)] for item in row[slice(*col_section)]]
+    return region
+
+
+def get_region_by_rc(puzzle, row, column):
     row_section = get_section(row)
     col_section = get_section(column)
     region = [item for row in puzzle[slice(*row_section)] for item in row[slice(*col_section)]]
     return region
+
+
+def get_missing(lst: set, acceptable_values: set) -> set:
+    """
+    Returns list of values missing from list
+    :return: list of values missing from list
+    :rtype: set
+    :param lst: said list
+    :param acceptable_values: all acceptable values (usually digits 1-9)
+
+    """
+    return acceptable_values.difference(lst)
+
+
+def get_empty_cells(puzzle, acceptable_values):
+    for i_row in range(len(puzzle)):
+        for j_cell in range(len(puzzle[i_row])):
+            if puzzle[i_row][j_cell] not in acceptable_values:
+                yield i_row, j_cell
+
+
+def get_empty_cells_in_region(puzzle, acceptable_values, i_min, j_min, i_max, j_max):
+    ijs = list(get_empty_cells(puzzle, acceptable_values))
+    coords = lambda ij: i_min <= ij[0] < i_max and j_min <= ij[1] < j_max
+    return filter(coords, ijs)
+
+
+def generate_empty_scratch():
+    return [[None for j in range(9)] for i in range(9)]
+
+def generate_scratch(puzzle, length, acceptable_values):
+    return [["{:^{}}".format(puzzle[i][j], length) if puzzle[i][j] in acceptable_values else "     "
+             for j in range(9)] for i in range(9)]
+
+########################################################################################################################
+# methods
+########################################################################################################################
+
+def find_single_missing(puzzle, acceptable_values):
+    """
+    Finds all cells with only one variant
+    """
+    length = 5
+    candidates = generate_scratch(puzzle, length, acceptable_values)
+    for i, j in get_empty_cells(puzzle, acceptable_values):
+        missing_row = get_missing(set(get_row(puzzle, i)), acceptable_values)
+        missing_col = get_missing(set(get_column(puzzle, j)), acceptable_values)
+        missing_region = get_missing(set(get_region_by_rc(puzzle, i, j)), acceptable_values)
+        acceptable_here = missing_col & missing_row & missing_region
+        if not acceptable_here:
+            raise Exception("Zero variants for empty cell")
+        elif len(acceptable_here) == 1:
+            (value, *rest) = acceptable_here
+            candidates[i][j] = "{:^{}}".format("->{}<-".format(value), length)
+            puzzle[i][j] = value
+        elif len(acceptable_here) == 2:
+            l_acc = list(acceptable_here)
+            candidates[i][j] = "{:^{}}".format("[{},{}]".format(*l_acc), length)
+    print_puzzle(candidates)
+    return puzzle
+
+
+def find_exclude_in_regions(puzzle, acceptable_values):
+    """
+    Finds only cell in the region that can contain given number
+    """
+    n_regions = 3
+    region_indexes = [get_region_indexes(i, 3) for i in range(n_regions)]
+    guesses = generate_empty_scratch()
+    for region_i in range(n_regions):
+        for region_j in range(n_regions):
+            print("region {} {}".format(region_i,region_j))
+            region_content = get_region(puzzle, region_i, region_j)
+            missing_from_region = get_missing(set(region_content), acceptable_values)
+            print("missing: ", missing_from_region)
+            empty_cells = list(get_empty_cells_in_region(puzzle, acceptable_values,
+                                                         region_indexes[region_i][0],
+                                                         region_indexes[region_j][0],
+                                                         region_indexes[region_i][1],
+                                                         region_indexes[region_j][1]))
+
+            for missing in missing_from_region:
+                # проверяем, где может стоять эта цифра
+                pass
+
+
+def solve(puzzle, acceptable_values):
+    c_puzzle = copy.deepcopy(puzzle)
+    rounds = 0
+    while True:
+        rounds += 1
+        print("round #{}".format(rounds))
+        new_puzzle = copy.deepcopy(c_puzzle)
+        new_puzzle = find_single_missing(new_puzzle, acceptable_values)
+        find_exclude_in_regions(new_puzzle, acceptable_values)
+        # print(new_puzzle)
+        if c_puzzle == new_puzzle:
+            return new_puzzle
+        c_puzzle = new_puzzle
+
+
+if __name__ == "__main__":
+    print("Puzzle:")
+    print_puzzle(sample.hard["puzzle"])
+    np = solve(sample.hard["puzzle"], sample.acceptable_values)
+    print("Solved:")
+    print_puzzle(np)
--- tests.py
+++ tests.py
@@ -3,16 +3,17 @@ import sample
 import sudoku
 
 
+
 class MyTestCase(unittest.TestCase):
 
     def test_column(self):
 
-        col0 = sudoku.get_column(sample.sudoku, 0)
+        col0 = sudoku.get_column(sample.easy["puzzle"], 0)
         self.assertEqual(col0, ['X', 'X', '8', '3', '4', 'X', '5', '2', 'X'])
 
     def test_row(self):
 
-        row0 = sudoku.get_row(sample.sudoku, 0)
+        row0 = sudoku.get_row(sample.easy["puzzle"], 0)
 
         self.assertEqual(row0, list("XXXX6824X"))
 
@@ -30,8 +31,23 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(sudoku.get_section(8), (6, 9))
 
     def test_regions(self):
-        reg0 = sudoku.get_region(sample.sudoku, 0, 0)
+        reg0 = sudoku.get_region_by_rc(sample.easy["puzzle"], 0, 0)
         self.assertEqual(reg0, ['X', 'X', 'X', 'X', 'X', '9', '8', '6', 'X'])
+
+    def test_missing(self):
+        col0 = set(sudoku.get_column(sample.easy["puzzle"], 0))
+        self.assertEqual(sudoku.get_missing(col0, sample.acceptable_values), {'1', '6', '9', '7'})
+
+    def test_yielding_empty_cells(self):
+        for i, j in sudoku.get_empty_cells(sample.easy["puzzle"], sample.acceptable_values):
+            self.assertEqual(sample.easy["puzzle"][i][j], "X")
+
+    def test_get_empty_cells_in_region(self):
+        em = list(sudoku.get_empty_cells_in_region(sample.easy["puzzle"], sample.acceptable_values,
+                                                   0, 0, 3, 3))
+        self.assertEqual(len(em), 6)
+
+
 
 if __name__ == '__main__':
     unittest.main()
